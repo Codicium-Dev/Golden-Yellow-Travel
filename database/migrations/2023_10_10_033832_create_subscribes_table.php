6433f0b10de298c9fcd981536b60277588d7a4d4
Zip <thantzinhtet2001@gmail.com>
2023-10-11 10:27:00 +0630

    fix

--- database/migrations/2023_10_10_033832_create_subscribes_table.php
+++ database/migrations/2023_10_10_033832_create_subscribes_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string("name")->nullable()->default("Unknown User");
             $table->string("email");
             $table->string("phone")->nullable();
-            $table->timestamps();
+            $table->auditColumns();
         });
     }
 
